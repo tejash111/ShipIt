b78a33331c3199a83e1eb41b4c5f27f50ebb1c91
tejash111 <tejashsinghrajput@gmail.com>
2025-07-17 17:57:58 +0530

    .

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -4,68 +4,65 @@ import axios from "axios";
 
 export const callRegisterApi = async (formData) => {
   const response = await axios.post(
-    "https://shipit1.onrender.com/api/user/register",
-    // "http://localhost:3000/api/user/register",
+    "http://localhost:3000/api/user/register",
     formData,
     {
-      withCredentials: true,
+      withCredentials: true, 
     }
   );
 
   return response.data;
 };
 
-export const callLoginApi = async (formData) => {
+export const callLoginApi = async (formData) =>{
   const response = await axios.post(
-    "https://shipit1.onrender.com/api/user/login",
-    // "http://localhost:3000/api/user/login",
+    "http://localhost:3000/api/user/login",
     formData,
     {
-      withCredentials: true
+      withCredentials:true
     }
   );
   return response.data
 }
 
-export const addNewTaskApi = async (formData) => {
+export const addNewTaskApi = async(formData)=>{
   const response = await axios.post(
-    "https://shipit1.onrender.com/api/user/add-new-task",
-    // "http://localhost:3000/api/user/add-new-task",
+    "http://localhost:3000/api/user/add-new-task",
     formData,
     {
-      withCredentials: true
-    }
+    withCredentials: true 
+  }
   )
   return response?.data;
 }
 
-export const fetchTackApi = async () => {
+export const fetchTackApi = async()=>{
   const response = await axios.get(
-    "https://shipit1.onrender.com/api/user/get-all-task/",
+    "http://localhost:3000/api/user/get-all-task/",
     {
-      withCredentials: true
+      withCredentials:true
     }
   )
   return response?.data
 }
 
-export const deleteTaskApi = async (getCurrentTaskId) => {
+export const deleteTaskApi = async(getCurrentTaskId)=>{
   const response = await axios.delete(
-    `https://shipit1.onrender.com/api/user/delete-task/${getCurrentTaskId}`,
+    `http://localhost:3000/api/user/delete-task/${getCurrentTaskId}`,
 
     {
-      withCredentials: true
+      withCredentials:true
     }
   )
   return response?.data
 }
 
-export const updateTaskApi = async (formData) => {
+export const updateTaskApi = async(formData)=>{
   const response = await axios.put(
-    `https://shipit1.onrender.com/api/user/update-task`,
+    `http://localhost:3000/api/user/update-task`,
     formData,
     {
-      withCredentials: true
+      withCredentials:true
     }
   )
   return response?.data
@@ -73,7 +70,7 @@ export const updateTaskApi = async (formData) => {
 
 export const logoutApi = async (req, res) => {
   const response = await axios.post(
-    "https://shipit1.onrender.com/api/user/logout",
+    "http://localhost:3000/api/user/logout",
     {},
     { withCredentials: true }
   );
