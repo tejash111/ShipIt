cd3eea6d4aa6f6ba03493bc2f79af34add44ee99
tejash111 <tejashsinghrajput@gmail.com>
2025-07-17 16:50:30 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ship-it-one.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
