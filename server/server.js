6e75681aa28893ea35284350dc5d7ba6e92ca3a8
tejash111 <tejashsinghrajput@gmail.com>
2025-07-17 15:50:12 +0530

    https cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "http://ship-it-one.vercel.app"
+      "https://ship-it-one.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
